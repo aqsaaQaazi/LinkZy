fc24b0a21dbd4a2fde959da3181b97789b642c78
aqsaaQaazi <aqsaakqaazi@gmail.com>
2024-12-01 04:13:41 +0500

    add meta head tag in pages.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 import RunTime from "@/components/app";
+import Head from "next/head";
 
 export default function Home() {
   return (
+<>
+
+ {/* SEO Head Component */}
+ <Head>
+        <title>LinkZy - Free and Fast URL Shortener</title>
+        <meta
+          name="description"
+          content="LinkZy lets you instantly shorten links with a hassle-free, fast, and shareable interface. No sign-up required, Simplify Link Management today.!"
+        />
+        <meta
+          name="keywords"
+          content="best free URL shortener for marketers ,URL shortener, link shortener, free short links, link trimmer, new link generator, new url generator tool, linkzy"
+        />
+
+        {/* favicon doesnt work anyways :/ */}
+        {/* <link rel="icon" href="/favicon.png" type="image/png" /> */}
+      </Head>
+
     <RunTime/>
    
+</>
   );
 }
